5ff654987bbcd0ecbf8eee09444e06d199ebf957
Eduardo Nunes <eduardocarvalho-1992@hotmail.com>
2020-04-01 10:54:17 +0100

    codigo vetor ordenado

--- Cidade.py
+++ Cidade.py
@@ -1,11 +1,10 @@
 class Cidade:
-    def __init__(self, nome):
+    def __init__(self, nome, distanciaObjetivo):
         self.nome = nome
         self.visitado = False
+        self.distanciaObjetivo = distanciaObjetivo
         self.adjacentes = []
-        
+    
     def addCidadeAdjacente(self, cidade):
         self.adjacentes.append(cidade)
-      
         
-
